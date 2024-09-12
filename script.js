c46a0f36d0fca4d9a2ef39eab27a714b38889501
Zena-art <zena.nazim@gmail.com>
2024-09-11 20:29:25 -0500

    Practical math

--- script.js
+++ script.js
@@ -60,4 +60,75 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 
   const isFirstNumberLarger = n1 > n4;
 
-  console.log(isFirstNumberLarger);
+  //Accomplish the following arithmetic chain:
+//Subtract the first number from the second number.
+
+const arithmeticChain1 = n1 - n2;
+
+//Multiply the result by the third number.
+
+const arithmeticChain2 = arithmeticChain1 * n3;
+
+//Find the remainder of dividing the result by the fourth number.
+
+const arithmeticChain3 = arithmeticChain2 % n4;
+
+console.log(arithmeticChain3);
+
+
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+
+const isUnder25 = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25;
+
+/*Part 2: Practical Math
+Let’s look at a more practical scenario.
+You are planning a cross-country road trip!
+
+The distance of the trip, in total, is 1,500 miles.
+Your car’s fuel efficiency is as follows:
+At 55 miles per hour, you get 30 miles per gallon.
+At 60 miles per hour, you get 28 miles per gallon.
+At 75 miles per hour, you get 23 miles per gallon.
+You have a fuel budget of $175.
+The average cost of fuel is $3 per gallon.
+*/
+
+const distance = 1500;
+const budget = 175;
+const fuelCost = 3;
+const gallonsOfFuel = budget / fuelCost;
+
+// How many gallons of fuel we need for the entire trip on certain speed
+const fuelPerHour55 = distance / 30;
+const fuelPerHour60 = distance / 28;
+const fuelPerHour75 = distance / 23;
+
+// Is the budget will be enough to cover the fuel expense?
+
+const costForTrip55 = fuelPerHour55 * fuelCost;
+const costForTrip60 = fuelPerHour60 * fuelCost;
+const costForTrip75 = fuelPerHour75 * fuelCost;
+
+
+// How long will the trip take in hours?
+
+const timeForTrip55 = distance / 55;
+const timeForTrip60 = distance / 60;
+const timeForTrip75 = distance / 75;
+
+if(costForTrip55 <= budget){
+  console.log(`Traveling at speed of 55 is withing your budget! You will spend $ ${costForTrip55}`)
+}else{
+  console.log(`You won't have enough money at this speed! Your cost will be$ ${costForTrip55}`);
+}
+
+if(costForTrip60 <= budget){
+  console.log(`Traveling at speed of 60 is withing your budget! You will spend $ ${costForTrip60}`)
+}else{
+  console.log(`You won't have enough money at this speed! Your cost will be$ ${costForTrip60}`);
+}
+if(costForTrip75 <= budget){
+  console.log(`Traveling at speed of 75 is withing your budget! You will spend $ ${costForTrip75}`)
+}else{
+  console.log(`You won't have enough money at this speed! Your cost will be $ ${costForTrip75}`);
+}
